0a377321f26413af1cdf28bf9db1d806d7b83d6d
annamori <annyon_25@yahoo.co.jp>
2017-02-16 16:38:44 +0000

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+before_action :correct_user, only: [:edit, :update]
+ 
   def new
      @user = User.new
   end
@@ -12,11 +14,31 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+   def edit
+    @user = User.find(params[:id])
+ 　 end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Updated your Plofile"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name, :email, :location, :password,
                                  :password_confirmation)
   end
+  
+   def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path if @user != current_user
+   end
+ 
 end
