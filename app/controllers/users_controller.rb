721be0b734cb37aa640ee3aabb55769fba58dd8a
annamori <annyon_25@yahoo.co.jp>
2017-02-18 05:40:56 +0000

    third commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 before_action :correct_user, only: [:edit, :update]
+
+  def show
+    @user = User.find(params[:id])
+  end
  
   def new
      @user = User.new
@@ -15,9 +19,9 @@ before_action :correct_user, only: [:edit, :update]
     end
   end
   
-   def edit
+  def edit
     @user = User.find(params[:id])
- 　 end
+  end
 
   def update
     @user = User.find(params[:id])
@@ -39,10 +43,6 @@ before_action :correct_user, only: [:edit, :update]
    def correct_user
     @user = User.find(params[:id])
     redirect_to root_path if @user != current_user
-   end
-  
-   def show
-    @user = User.find(params[:id])
    end
    
 end
